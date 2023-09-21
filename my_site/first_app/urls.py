89f638284a318b884f620b0f1f2d67d6b3177dda
Frank Entsie <frank.entsie301@gmail.com>
2023-09-21 10:18:14 +0900

    rendering html templates using the views and connecting it to the project level template

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("<int:num_page>",views.num_page_view),
-   path("<str:topic>/", views.news_view,name="topics-page"),
+path("",views.simple_view)
+#    path("<int:num_page>",views.num_page_view),
+#    path("<str:topic>/", views.news_view,name="topics-page"),
 
    #path("<int:num1>/<int:num2>",views.add_view)
    
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -3,30 +3,63 @@ from django.http.response import HttpResponse,HttpResponseNotFound,Http404,HttpR
 from django.urls import reverse
 # Create your views here.
 
-articles={"sports":"sports page","finance":"finance page","history":"history page"}
+def simple_view(request):
+   return render(request,"first_app/example.html",)
 
-def news_view(request,topic):
-   try:
-      result=articles[topic]
-      return HttpResponse(articles[topic])
-   except:
-      result="No page for this topic"
-      raise Http404(result)
 
-# def finance_view(request):
-#    return HttpResponse(articles["finance"])
 
-def add_view(request,num1,num2):
-    result = num1 + num2
-    return HttpResponse(str(result))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# articles={"sports":"sports page","finance":"finance page","history":"history page"}
+
+# def news_view(request,topic):
+#    try:
+#       result=articles[topic]
+#       return HttpResponse(articles[topic])
+#    except:
+#       result="No page for this topic"
+#       raise Http404(result)
+
+# # def finance_view(request):
+# #    return HttpResponse(articles["finance"])
+
+# def add_view(request,num1,num2):
+#     result = num1 + num2
+#     return HttpResponse(str(result))
  
-def num_page_view(request,num_page):
-   try:
-      topics_list=list(articles.keys())
-      topic=topics_list[num_page]
+# def num_page_view(request,num_page):
+#    try:
+#       topics_list=list(articles.keys())
+#       topic=topics_list[num_page]
       
-      webpage = reverse("topics-page",args=[topic])
-      return HttpResponseRedirect(webpage)
-   except:
-      raise Http404("page not found")
+#       webpage = reverse("topics-page",args=[topic])
+#       return HttpResponseRedirect(webpage)
+#    except:
+#       raise Http404("page not found")
     
